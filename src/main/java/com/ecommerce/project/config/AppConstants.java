585e3059b978e1e937e37eb3be5d746b959a26ce
revin99 <revingohil@gmail.com>
2025-12-08 00:35:21 +0530

    Adding product module - creating mode, controller, service, repository, dto classes

--- src/main/java/com/ecommerce/project/config/AppConstants.java
+++ src/main/java/com/ecommerce/project/config/AppConstants.java
@@ -6,4 +6,6 @@ import org.springframework.context.annotation.Bean;
 public class AppConstants {
     public static final String PAGE_NUMBER = "0";
     public static final String PAGE_SIZE = "50";
+    public static final String SORT_CATEGORIES_BY = "categoryId";
+    public static final String SORT_DIR = "asc";
 }
