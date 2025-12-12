ad1d4c366aa77bff670787aae8f1b6ae941d29e3
revin99 <revingohil@gmail.com>
2025-12-12 17:48:06 +0530

    Creating security classes for JWT Creation, validation and error handling

---
+++ src/main/java/com/ecommerce/project/security/jwt/LoginResponse.java
@@ -0,0 +1,23 @@
+package com.ecommerce.project.security.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class LoginResponse {
+    private String jwtToken;
+
+    private String username;
+    private List<String> roles;
+
+    public LoginResponse(String username, List<String> roles, String jwtToken){
+        this.username=username;
+        this.roles=roles;
+        this.jwtToken=jwtToken;
+    }
+
+
+}
