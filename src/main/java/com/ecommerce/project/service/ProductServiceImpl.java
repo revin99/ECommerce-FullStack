e83c1885b3f6204ce65a555b0a9ba1ec69af6e73
revin99 <revingohil@gmail.com>
2025-12-17 15:29:50 +0530

    Product updates are being propogated to user's cart

--- src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -230,6 +230,9 @@ public class ProductServiceImpl implements ProductService{
         Product productFromDb = productRepository.findById(productId)
                 .orElseThrow(()->new ResourceNotFoundException("Product","productId",productId));
 
+        List<Cart> carts = cartRepository.findCartsByProductId(productId);
+        carts.forEach(cart -> cartService.deleteProductFromCart(cart.getCartId(),productId));
+
         productRepository.deleteById(productId);
         return modelMapper.map(productFromDb,ProductDTO.class);
     }
